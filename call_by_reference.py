b01f0a6a3d6ec8f88cd4d5d1292fec551a913788
Vaishnavi <vaishnavihonrao13@gmail.com>
2026-08-26 10:57:54 +0530

    function(call by reference)

---
+++ call_by_reference.py
@@ -0,0 +1,7 @@
+def change_value(x):
+    x=x+10
+    print("inside functio",x)
+a=5
+change_value(a)
+print("outside function :",a)
+
---
+++ defalt_argument.py
@@ -0,0 +1,7 @@
+def function(n1,n2=20):
+    print("n1:",n1)
+    print("n2:",n2)
+print("passing only one argument")
+function(40)
+print("passing two arguments")
+function(40,60)
